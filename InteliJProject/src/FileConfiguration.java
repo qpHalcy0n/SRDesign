84377bc6f70409a14cd508df549e8a26e544309f
DanLain <daniellain@Daniels-MacBook-Pro.local>
2014-02-08 22:23:04 -0600

    Updated Config files

--- InteliJProject/src/FileConfiguration.java
+++ InteliJProject/src/FileConfiguration.java
@@ -6,7 +6,6 @@ import java.io.File;
 public class FileConfiguration {
     private File rawSTLFile;
     private File subsetSTL;
-    private File gCode;
 
     private ExtruderConfiguration extruderConfiguration;
     private MaterialConfiguration materialConfiguration;
--- InteliJProject/src/SubsetConfiguration.java
+++ InteliJProject/src/SubsetConfiguration.java
@@ -1,9 +1,14 @@
+import java.io.File;
+import java.util.List;
 
 public class SubsetConfiguration {
 	private double startZ;
 	private double endZ;
+    File amfFile;
+    File gCodeFile;
     private PrinterConfiguration printerConfiguration;
     private PrintConfiguration printConfiguration;
+    private List<FileConfiguration> fileConfigurations;
 
 
 }
