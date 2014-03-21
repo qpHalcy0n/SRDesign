efe8b6cdab873cf1f9af273c958d78ea48076b3a
DanLain <daniellain@192.168.0.16>
2014-03-21 06:58:50 -0500

    Created Test Package

---
+++ Ink3DNetBeans/test/ink3d/UserInterface/Database/XmlDatabase/DeleteExtruderConfigurationCommandTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ink3d.UserInterface.Database.XmlDatabase;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author daniellain
+ */
+public class DeleteExtruderConfigurationCommandTest {
+    
+    public DeleteExtruderConfigurationCommandTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of execute method, of class DeleteExtruderConfigurationCommand.
+     */
+    @Test
+    public void testExecute() {
+        System.out.println("execute");
+        DeleteExtruderConfigurationCommand instance = null;
+        instance.execute();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
