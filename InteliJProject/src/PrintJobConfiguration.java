7322e80a3f811ab82d9d8de10d0a3df2c0705cb5
DanLain <daniellain@Daniels-MacBook-Pro.local>
2014-03-20 22:17:09 -0500

    removed InteliJ Project

--- InteliJProject/src/PrintJobConfiguration.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-import java.util.List;
-
-
-public class PrintJobConfiguration {
-    private PrinterConfiguration printerConfiguration;
-    private List <SubsetConfiguration> subsetConfigurationList;
-	
-}
