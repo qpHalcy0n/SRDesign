208b122867e3cfcf47b3fb8802a3bc5f6752a2f4
tde90 <edmondson.tim@gmail.com>
2014-05-14 02:35:04 -0500

    Imports now use OpenScad to convert to ascii stl format (very ugly temporary fix)

--- Ink3DNetBeans/src/ink3d/UserInterface/Database/XmlDatabase/ImportStlFileCommand.java
+++ Ink3DNetBeans/src/ink3d/UserInterface/Database/XmlDatabase/ImportStlFileCommand.java
@@ -7,7 +7,9 @@
 package ink3d.UserInterface.Database.XmlDatabase;
 
 import ink3d.UserInterface.Database.CommandStructure;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
@@ -28,15 +30,44 @@ public class ImportStlFileCommand extends CommandStructure{
     @Override
     public void execute() {
         try {
+
+            // Create temp scad file
+            File tempFile = new File("temp.scad");
+            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+            String inputFilePath = stlFile.getAbsolutePath().replaceAll("\\\\", "\\\\\\\\");
+            writer.append("import(\"" + inputFilePath + "\");");
+            writer.close();
+
+            // Create command to use openscad
+            String baseDir = new File("").getAbsolutePath();
+            String destPath = baseDir + "/Database/Files/"
+                + stlFile.getName().substring(0, stlFile.getName().length() - 4)
+                + ".stl";
+            String command = baseDir + "/third-party/openscad/openscad.exe -o "
+                + "\"" + destPath + "\" \"" + tempFile.getAbsolutePath() + "\"";
+            
+            // Use openscad to convert STL file to ascii
+            Process openScadProcess = Runtime.getRuntime().exec(command);
+            openScadProcess.waitFor();
+
+            // Delete temp file
+            tempFile.delete();
+            
+            /*
             StringBuilder sb = new StringBuilder();
             sb.append("./Database/Files/");
             sb.append(stlFile.getName().substring(0, stlFile.getName().length() - 4));
             sb.append(".stl");
             Files.copy(stlFile.toPath(), (new File(sb.toString())).toPath(), StandardCopyOption.REPLACE_EXISTING);
             stlFile = null;
+            */
+
             System.gc();
             result = Boolean.TRUE;
         } catch (IOException ex) {
+            Logger.getLogger(ImportStlFileCommand.class.getName()).log(Level.SEVERE, null, ex);
+            result = Boolean.FALSE;
+        } catch (InterruptedException ex) {
             Logger.getLogger(ImportStlFileCommand.class.getName()).log(Level.SEVERE, null, ex);
             result = Boolean.FALSE;
         }
